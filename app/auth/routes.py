bdbe4fd4f4738bcb116b4edb82762e1b6ca7c117
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2023-03-21 11:38:33 -0500

    validar de que el usuario y el email no esten en uso en la ruta de update user, hecho

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -105,11 +105,20 @@ def updateUser(user_id):
     data = request.get_json()
     user = Users.query.filter_by(id=user_id).first()
 
-
-
-    if data.get('username') and data.get('email') is not None:
+    # Validar si el nombre de usuario ya existe
+    if data.get('username'):
+        existing_user = Users.query.filter(Users.username == data['username']).first()
+        if existing_user and existing_user.id != user_id:
+            return jsonify({'message': 'El nombre de usuario ya existe. Por favor, elija otro.'}), 400
         user.username = data['username']
+
+    # Validar si el correo electrónico ya existe
+    if data.get('email'):
+        existing_user = Users.query.filter(Users.email == data['email']).first()
+        if existing_user and existing_user.id != user_id:
+            return jsonify({'message': 'El correo electrónico ya existe. Por favor, elija otro.'}), 400
         user.email = data['email']
+
 
     if data.get('avatar') is not None:
         user.avatar = data['avatar']
@@ -154,6 +163,105 @@ def updateUser(user_id):
 
     db.session.commit()
     return jsonify({'message': 'user updated successfully'})
+
+
+
+
+
+
+@auth_bp.route("/update-user", methods=["PUT"])
+def update_user():
+    try:
+        # Obtenemos el objeto json que viene desde el frontend
+        if request.data:
+            userDatas = json.loads(request.data)
+
+            # Declaramos las variables necesarias para actualizar la información
+            user_id = userDatas['user_id']
+            username = userDatas['username']
+            email = userDatas['email']
+            avatar = userDatas['avatar']
+
+            user = Users.query.filter_by(id=user_id).first()
+
+            if user is None:
+                return jsonify({
+                    "message": "User not found"
+                }, 404)
+
+            # Check if the new username and email already exist in the db
+            if username != user.username:
+                existing_username = Users.query.filter_by(username=username).first()
+                if existing_username is not None:
+                    return jsonify({
+                        "message": "Username already exists"
+                    }, 400)
+                else:
+                    user.username = username
+
+            if email != user.email:
+                existing_email = Users.query.filter_by(email=email).first()
+                if existing_email is not None:
+                    return jsonify({
+                        "message": "Email already exists"
+                    }, 400)
+                else:
+                    user.email = email
+
+            if avatar:
+                photo = avatar
+                photoAppended = {}
+
+                os.makedirs(file_dir, exist_ok=True)
+                result = re.search("data:image/(?P<ext>.*?);base64,(?P<data>.*)", photo['imagePath'], re.DOTALL)
+
+                if result:
+                    image_full_name = photo['imagenFullName']
+                    image_name = photo['imageName']
+                    image_ext = photo['imageExt']
+                    image_size = photo['imageSize']
+
+                    #se le contatena al nombre del archivo una fecha con tiempo
+                    dateNow = datetime.now()
+                    image_path = image_name + '-' + dateNow.strftime('%Y%m%d%H%M%S')
+
+                    os.makedirs(file_dir, exist_ok=True)
+                    photo_path = os.path.join(file_dir, (image_path + image_ext))
+
+                    #creación de un groupdict
+                    data = result.groupdict().get("data")
+
+                    #bases64 decodificación
+                    img = base64.urlsafe_b64decode(data)
+
+                    with open(photo_path, "wb") as f:
+                        f.write(img)
+
+                    photoAppended = {
+                        'imagenFullName': image_full_name,
+                        'imageName': image_name,
+                        'imageExt': image_ext,
+                        'imageSize': image_size,
+                        'imagePath': image_path,
+                        'imageServer': True
+                    }
+
+                user.avatar = photoAppended
+
+            db.session.commit()
+            return jsonify({
+                "message": "User updated successfully"
+            }, 200)
+
+        return jsonify({
+            "message": "User could not be updated"
+        }, 400)
+    except Exception as e:
+        return jsonify({
+            "message": "Error updating user",
+            "error": str(e)
+        }, 500)
+
 
 
 @auth_bp.route('/login', methods=['POST'])
