4cf51389a92f9bc00391488552d5878b5f578634
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2023-03-23 16:53:34 -0500

    bd y fotos de perfil

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-03-2023 a las 15:18:39
+-- Tiempo de generación: 23-03-2023 a las 22:53:06
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -40,7 +40,11 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `post_id`, `content`, `user_id`, `created_at`) VALUES
-(105, 2174, 'Hola soy anonimo\n', 75, '2023-03-13 20:21:03');
+(105, 2174, 'Hola soy anonimo\n', 75, '2023-03-13 20:21:03'),
+(113, 2174, 'hola', 3, '2023-03-23 19:58:47'),
+(114, 2174, 'gf', 3, '2023-03-23 20:01:25'),
+(117, 2174, 'Hola\n', 3, '2023-03-23 20:47:04'),
+(121, 2174, 'esta pub es mia', 2, '2023-03-23 20:54:08');
 
 -- --------------------------------------------------------
 
@@ -61,8 +65,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `user_id`, `created_at`) VALUES
-(2174, 'The Thing (1982)', 'Un equipo científico de la Antártida descubre un ente extraño que podría ser letal para la humanidad, un extraterrestre que puede duplicar otras formas de vida.', 2, '2023-03-13 19:06:56'),
-(2177, 'Hola', 'Hola que tal', 3, '2023-03-14 14:06:10');
+(2174, 'The Thing (1982)', 'Un equipo científico de la Antártida descubre un ente extraño que podría ser letal para la humanidad, un extraterrestre que puede duplicar otras formas de vida.', 2, '2023-03-13 19:06:56');
 
 -- --------------------------------------------------------
 
@@ -84,12 +87,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `avatar`) VALUES
-(1, 'JhonnysDS', 'j_honydavid@hotmail.com', 'pbkdf2:sha256:260000$LfXUvNQQJRRaS86T$94e992efbb910878ad5f04abfef48d6ec4c8d16e5e9bf247cc0482280ad4eff3', '2023-01-13 03:26:27', ''),
-(2, 'jds', 'jds@mail.com', 'pbkdf2:sha256:260000$pn7Rzpk51PpLxKXz$350d11bb0c4a24acf7f00004ed31ea1b4a9860f29b7b83abfd60c972cb9eb0c5', '2023-01-16 18:34:18', ''),
-(3, 'kal', 'kal@gmail.com', 'pbkdf2:sha256:260000$WwOMoRt1SnRBnDrf$222b0c9beaa267acb3bb16174bae9a80d2b87fcfc27bfbd062e60bc4f3343b0d', '2023-01-16 16:58:40', ''),
-(30, '', '', 'pbkdf2:sha256:260000$8T6MCEr6mqjTX7sN$d8b6b44094b18e366ddf551f66d2070eeef3135f1858408cf198f199f7fec1fd', '2023-02-09 21:11:16', ''),
-(102, 'anonimo', 'anonimo@gmail.com', 'pbkdf2:sha256:260000$xVLQWhss8NTlp4UQ$7e969c5272914942862ba3103c2661c93767ebf9c9239e8607375e6ee87e4db0', '2023-03-16 16:20:17', '{\'imagenFullName\': \'anonimous.jpg\', \'imageName\': \'anonimous\', \'imageExt\': \'.jpg\', \'imageSize\': 921910, \'imagePath\': \'anonimous-20230316112017\', \'imageServer\': true}'),
-(104, 'gato', 'gato@gmail.com', 'pbkdf2:sha256:260000$LW1dZC4HcFDI7l26$934a6418eb66200636ec35f863bd0b3c5eb80440cc29527d9587225057f79632', '2023-03-16 19:33:39', '{\'imagenFullName\': \'cat-gbfd27a440_1280.jpg\', \'imageName\': \'cat-gbfd27a440_1280\', \'imageExt\': \'.jpg\', \'imageSize\': 320729, \'imagePath\': \'cat-gbfd27a440_1280-20230316143339\', \'imageServer\': True}');
+(1, 'JhonnysDS', 'j_honydavid@hotmail.com', 'pbkdf2:sha256:260000$LfXUvNQQJRRaS86T$94e992efbb910878ad5f04abfef48d6ec4c8d16e5e9bf247cc0482280ad4eff3', '2023-01-13 03:26:27', NULL),
+(2, 'jds', 'jds@mail.com', 'pbkdf2:sha256:260000$pn7Rzpk51PpLxKXz$350d11bb0c4a24acf7f00004ed31ea1b4a9860f29b7b83abfd60c972cb9eb0c5', '2023-01-16 18:34:18', '{\'imagenFullName\': \'cat-gbfd27a440_1280.jpg\', \'imageName\': \'cat-gbfd27a440_1280\', \'imageExt\': \'.jpg\', \'imageSize\': 320729, \'imagePath\': \'cat-gbfd27a440_1280-20230323162002\', \'imageServer\': \'true\'}'),
+(3, 'kalel Morningstar', 'kal@gmail.com', 'pbkdf2:sha256:260000$ZS1GyM3ox19SQYi4$3c56d1b05c1e80aa43562cc9d0f1afcf46eae2ea7471bb60a5847e59f6bfe08a', '2023-01-16 16:58:40', '{\'imagenFullName\': \'84248.png\', \'imageName\': \'84248\', \'imageExt\': \'.png\', \'imageSize\': 205680, \'imagePath\': \'84248-20230323161833\', \'imageServer\': \'true\'}'),
+(30, '', '', 'pbkdf2:sha256:260000$8T6MCEr6mqjTX7sN$d8b6b44094b18e366ddf551f66d2070eeef3135f1858408cf198f199f7fec1fd', '2023-02-09 21:11:16', NULL),
+(102, 'anonimo', 'anonimo@gmail.com', 'pbkdf2:sha256:260000$LVwmW11MZft8BhYc$e836f2080ddda5e076faeabb801b1456414bf09ef653d619e2239372f90e8ed8', '2023-03-16 16:20:17', '{\'imagenFullName\': \'anonimous.jpg\', \'imageName\': \'anonimous\', \'imageExt\': \'.jpg\', \'imageSize\': 921910, \'imagePath\': \'anonimous-20230323162043\', \'imageServer\': \'true\'}'),
+(104, 'gato', 'gato@hotmail.com', 'pbkdf2:sha256:260000$LW1dZC4HcFDI7l26$934a6418eb66200636ec35f863bd0b3c5eb80440cc29527d9587225057f79632', '2023-03-16 19:33:39', '{\'imagenFullName\': \'cat-gbfd27a440_1280.jpg\', \'imageName\': \'cat-gbfd27a440_1280\', \'imageExt\': \'.jpg\', \'imageSize\': 320729, \'imagePath\': \'cat-gbfd27a440_1280-20230323162124\', \'imageServer\': \'true\'}');
 
 --
 -- Índices para tablas volcadas
@@ -126,19 +129,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
 
 --
 -- AUTO_INCREMENT de la tabla `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2183;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2184;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
 
 --
 -- Restricciones para tablas volcadas
